2ab0cbb6256401727304a5090f3926cc3245d8c4
adrianbuda <buda646@gmail.com>
2018-03-27 17:38:44 +0300

    update

---
+++ app/src/main/java/com/example/adrian/login_and_register/Register.java
@@ -0,0 +1,35 @@
+package com.example.adrian.login_and_register;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jgabrielfreitas.core.BlurImageView;
+
+public class Register extends AppCompatActivity {
+
+    TextView existingAccount;
+    BlurImageView mybackground;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mybackground=findViewById(R.id.myBackground);
+        mybackground.setBlur(2);
+
+
+        existingAccount = findViewById(R.id.loginRedirect);
+        existingAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
